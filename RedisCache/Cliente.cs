440aacf10723b92e62beb490771790fd3fda99bd
pedrobernardo27 <Josepedrobernardo@hotmail.com>
2023-11-01 10:25:37 -0300

    Add project files.

---
+++ RedisCache/Cliente.cs
@@ -0,0 +1,14 @@
+﻿namespace RedisCache
+{
+    public class Cliente
+    {
+        public string Cpf { get; set; }
+        public string Documento { get; set; }
+        public string Nome { get; set; }
+
+        public Cliente(string key) 
+        {
+            Cpf = key;
+        }    
+    }
+}
---
+++ RedisCache/Program.cs
@@ -0,0 +1,19 @@
+﻿using RedisCache;
+using ServiceStack.Redis;
+
+var host = "localhost:6379";
+var cpf1 = "12345678910";
+var cpf2 = "98765432150";
+
+var cliente1 = new Cliente(cpf1) { Documento = "479864202", Nome = "Pedro Bernardo" };
+var cliente2 = new Cliente(cpf2) { Documento = "123123123", Nome = "Rodrigo Soledade" };
+
+using (var redisCliente = new RedisClient(host))
+{
+    redisCliente.Set<Cliente>(cliente1.Cpf, cliente1);
+    redisCliente.Set<Cliente>(cliente2.Cpf, cliente2);
+
+    var clienteX = redisCliente.Get<Cliente>(cpf1.ToString());
+
+    Console.WriteLine($"cpf = {clienteX.Cpf}, Documento = {clienteX.Documento}" );
+}
